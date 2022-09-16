0eb5d78307b41dc4b1e126334c225157f5add658
AndreBarroso <andre_barroso17@hotmail.com>
2022-09-16 15:36:58 -0300

    feat: Variaveis, Condicionais e operadores aritméticos

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	
+	"modulo/oparacoesAritimeticas"
+	"fmt"
+	
+
+)
+
+func main() {
+	a := oparacoesAritimeticas.Soma(5,2) 
+	fmt.Println(a)
+
+	
+}
---
+++ oparacoesAritimeticas/oparacoesAritimeticas.go
@@ -0,0 +1,18 @@
+package oparacoesAritimeticas
+
+func Soma(a, b int) (int){
+	return a + b
+}
+
+func Subtracao(a, b int) (int){
+	return a - b
+}
+
+func Multiplicacao(a, b int) (int){
+	return a - b
+}
+
+func Mod(a, b int) (int){
+	return a % b
+}
+
